3b4d6efdcbd81e8e8eba8c4b0dccc0e0a97b249b
dylann97 <d.xavero@hotmail.com>
2025-06-16 13:08:07 +0200

    🚧 Add types

---
+++ types/articles.ts
@@ -0,0 +1,9 @@
+// Type de base avec les propriétés communes
+export interface BlogArticle {
+  slug: string;
+  title: string;
+  image: string;
+  date: string;
+  author: string;
+  content: string;
+}
---
+++ types/index.ts
@@ -0,0 +1,1 @@
+type CardColor = "blue" | "green" | "red" | "yellow";
